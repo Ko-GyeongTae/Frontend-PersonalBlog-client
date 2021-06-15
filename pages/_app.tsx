d09ed434c7a99501e653ec292f67c98dff929892
Ko-GyeongTae <kokt0203@naver.com>
2021-06-15 20:00:06 +0900

    <EDIT> pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import { AppProps } from 'next/app';
 import Top from '../src/components/Top';
+import 'semantic-ui-css/semantic.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div style={{ width: 1000, margin: '0 auto'}}>
+    <div style={{ width: 1000, margin: '0 auto', alignContent: 'center'}}>
       <Top />
       <Component {...pageProps} />
     </div>
