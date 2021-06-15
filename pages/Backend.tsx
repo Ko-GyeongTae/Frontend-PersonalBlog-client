307c07ac5880e4eedcaa13cef53c3e4a46227f87
Ko-GyeongTae <kokt0203@naver.com>
2021-06-15 19:59:25 +0900

    <ADD> Pages

---
+++ pages/Backend.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Grid, Image } from 'semantic-ui-react'
+export default function Ios() {
+    return (
+        <div>
+            <Grid columns={2} divided>
+                <Grid.Row>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{marginBottom: 20}}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
+    )
+}
