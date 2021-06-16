a9ee9dd00cc8f5be51f7ef99b6a2d5c0c960232c
Ko-GyeongTae <kokt0203@naver.com>
2021-06-16 16:01:05 +0900

    <EDIT>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import { AppProps } from 'next/app';
 import Top from '../src/components/Top';
+import Bottom from '../src/components/Bottom';
 import 'semantic-ui-css/semantic.min.css';
+import style from './app.module.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div style={{ width: 1000, margin: '0 auto', alignContent: 'center'}}>
-      <Top />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className={`${style.side} card`}>
+        
+      </div>
+      <div style={{ width: 1000, margin: '0 auto', alignContent: 'center' }}>
+        <Top />
+        <Component {...pageProps} />
+        <Bottom />
+      </div>
+    </>
   )
 }
 
