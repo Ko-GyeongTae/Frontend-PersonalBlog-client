f2ab6f71372709e06ce774e85b1e8addeb34d800
Ko-GyeongTae <kokt0203@naver.com>
2021-06-17 15:21:38 +0900

    <ADD> Profile Image

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,8 +1,10 @@
+import style from './Profile.module.css'
+
 export default function Profile() {
     return (
-        <div style={{ width: 1000, height: 2000, backgroundColor: 'green'}}>
+        <div style={{ width: 1000, height: 2000 }}>
             <div >
-
+                <img className={style.img} src={'https://avatars.githubusercontent.com/u/61784451?v=4'}></img>
             </div>
         </div>
     )
