f208123db22260aa74e8b7e08f4f9c648fafc730
Ko-GyeongTae <kokt0203@naver.com>
2021-06-17 22:14:46 +0900

    <EDIT> Head | Profile

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,8 +1,12 @@
 import style from './Profile.module.css'
+import Head from 'next/head'
 
 export default function Profile() {
     return (
         <div style={{ width: 1000, height: 2000 }}>
+            <Head>
+                <title>NEON | Profile</title>
+            </Head>
             <div >
                 <img className={style.img} src={'https://avatars.githubusercontent.com/u/61784451?v=4'}></img>
             </div>
