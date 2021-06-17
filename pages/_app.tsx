2cd35448785d6c548651e0675575571fa28eeb6c
Ko-GyeongTae <kokt0203@naver.com>
2021-06-17 12:17:45 +0900

    <ADD> Sidebar Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,26 +3,12 @@ import Top from '../src/components/Top';
 import Bottom from '../src/components/Bottom';
 import 'semantic-ui-css/semantic.min.css';
 import style from './app.module.css';
+import Sidebar from '../src/components/Sidebar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <div style={{ width: '1000, auto', height: 'auto' }}>
-        <div className={ style.side }>
-          <ul>
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Home</a>
-            </li>
-          </ul>
-        </div>
+        <Sidebar />
         <div style={{ width: 1000, margin: '0 auto', alignContent: 'center' }}>
           <Top />
           <Component {...pageProps} />
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import style from './Sidebar.module.css'
+
+export default function Sidebar() {
+
+    return (
+        <div className={ style.side }>
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Home</a>
+            </li>
+          </ul>
+        </div>
+    )
+}
