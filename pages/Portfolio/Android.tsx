f07a9621a9cc0a4614ce1dad2056150047229909
Ko-GyeongTae <kokt0203@naver.com>
2021-06-17 13:46:28 +0900

    <EDIT> Route

---
+++ pages/Portfolio/Android.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import Head from 'next/head'
+import { Grid, Image } from 'semantic-ui-react'
+export default function Ios() {
+    return (
+        <div style={{ marginTop: 40 }}>
+            <Head>
+                <title>NEON | Android</title>
+            </Head>
+            <Grid columns={2} divided>
+                <Grid.Row>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                    <Grid.Column style={{ marginBottom: 20 }}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
+    )
+}
