1594342de35e61e475c48ee6a888f4911d46876e
Ko-GyeongTae <kokt0203@naver.com>
2021-06-21 23:42:46 +0900

    <ADD> getBadge API

---
+++ pages/api/getBadge.ts
@@ -0,0 +1,14 @@
+export const getBadges = (name) => {
+    if(name === 'javascript'){
+        return 'https://d1telmomo28umc.cloudfront.net/media/public/badges/js.png'
+    } 
+    if(name === 'go'){
+        return 'https://d1telmomo28umc.cloudfront.net/media/public/badges/goBadge.png'
+    }
+    if(name === 'typescript'){
+        return 'https://d1telmomo28umc.cloudfront.net/media/public/badges/typescript.png'
+    }
+    if(name === 'python'){
+        return 'https://d1telmomo28umc.cloudfront.net/media/public/badges/python_l9Src2C.png'
+    }
+}
