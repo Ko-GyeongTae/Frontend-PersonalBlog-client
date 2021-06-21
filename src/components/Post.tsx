f604644e649487a0dbb4df86337f65761a6e7f4b
Ko-GyeongTae <kokt0203@naver.com>
2021-06-21 23:43:10 +0900

    <EDIT> Post component & css

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,16 +1,19 @@
+import { getBadges } from '../../pages/api/getBadge'
 import style from './Post.module.css'
 
-export default function Post() {
+export default function Post(props) {
 
     return (
-        <div className={style.post}>
-               <div className={style.image}>
-                <img src={'/images/info.png'} alt='logo' style={{ display: 'block', width: 140 }} />
+        <a href={`${props.html_url}`}>
+            <div className={style.post}>
+                <div className={style.image}>
+                    <img src={`${getBadges(props?.language?.toLowerCase())}`} alt='logo' style={{ display: 'block', width: 140 }} />
+                </div>
+                <div className={style.content}>
+                    <h2>{props.name}</h2>
+                    <p>{props.description}</p>
+                </div>
             </div>
-               <div className={style.content}>
-                <h2>Title</h2>
-                <p>description</p>
-               </div>
-        </div>
+        </a>
     )
 }
