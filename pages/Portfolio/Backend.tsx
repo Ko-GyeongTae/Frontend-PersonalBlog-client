66816cd333f3028989b4a3b9df00f047005332e8
Ko-GyeongTae <kokt0203@naver.com>
2021-06-22 09:30:23 +0900

    <EDIT> Backend

--- pages/Portfolio/Backend.tsx
+++ pages/Portfolio/Backend.tsx
@@ -1,66 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Loader } from 'semantic-ui-react'
 import Post from '../../src/components/Post'
+import axios from 'axios';
 
 export default function Ios() {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const preLoad = async () => {
+        console.log(process.env.baseURL)
+        await axios.get(`http://122.34.166.121:5010/crawl/category/B`)
+            .then(res => {
+                console.log(res.data);
+                setPosts(res.data);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }
+
+    useEffect(() => {
+        setIsLoading(true);
+        preLoad();
+    }, [])
+
     return (
         <div style={{ marginTop: 40 }}>
             <Head>
                 <title>NEON | Backend</title>
             </Head>
-            <Grid columns={2} divided>
-                <Grid.Row>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                    <Grid.Column style={{ marginBottom: 20 }}>
-                        <Post />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+            {isLoading && (
+                <div style={{ padding: "300px 0" }}>
+                    <Loader inline="centered" active>
+                        Loading
+                    </Loader>
+                </div>
+            )}
+            {!isLoading && (
+                <Grid columns={2} divided>
+                    <Grid.Row>
+                        {posts.map(post => (
+                            console.log(post),
+                            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                <Post
+                                    key={post.id}
+                                    name={post.name}
+                                    description={post.description}
+                                    html_url={post.html_url}
+                                    language={post.language}
+                                />
+                            </Grid.Column>
+                        ))}
+                    </Grid.Row>
+                </Grid>
+            )}
         </div>
     )
 }
